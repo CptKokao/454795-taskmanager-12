8e8adafbed750e06de944733c60a9c526fc881f9
CptKokaoHome <pro-markov@yamdex.ru>
2020-08-02 20:05:05 +0300

    pазбивает на модули + конфиг webpack

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import {createMenuTemplate} from './view/menu.js';
+import {createFilterTemplate} from './view/filter.js';
+import {createSortTemplate} from './view/sort.js';
+import {createCartTemplate} from './view/cart.js';
+import {createCartEditTemplate} from './view/cart-edit.js';
+import {createLoadingTemplate} from './view/loader.js';
+
+
+const render = (container, place, template) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const mainElement = document.querySelector(`.main`);
+const mainControlElement = mainElement.querySelector(`.main__control`);
+const boardElement = document.querySelector(`.board`);
+const boardTaskElement = boardElement.querySelector(`.board__tasks`);
+
+render(mainControlElement, `afterbegin`, createMenuTemplate());
+render(mainElement, `afterbegin`, createFilterTemplate());
+render(boardElement, `afterbegin`, createSortTemplate());
+for (let i = 0; i < 3; i++) {
+  render(boardTaskElement, `afterbegin`, createCartTemplate());
+}
+render(boardTaskElement, `afterbegin`, createCartEditTemplate());
+render(boardElement, `beforeend`, createLoadingTemplate());
---
+++ src/view/loader.js
@@ -0,0 +1,9 @@
+export const createLoadingTemplate = () => {
+  return (
+    `<section class="board container">
+      <p class="board__no-tasks">
+        Loading...
+      </p>
+    </section>`
+  );
+};
