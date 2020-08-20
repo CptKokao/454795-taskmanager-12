483456b16df5ca8c199141aea0b4c418468fdb50
CptKokaoHome <pro-markov@yamdex.ru>
2020-08-20 08:37:32 +0300

    Добавляет наследование для простых компонентов

--- src/view/no-tasks.js
+++ src/view/no-tasks.js
@@ -1,28 +1,13 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createNoTasksTemplate =
     `<p class="board__no-tasks">
       Click  «ADD NEW TASKS» in menu to create your first task
     </p>`;
 
-export default class NoTasks {
-  constructor() {
-    this._element = null;
-  }
+export default class NoTasks extends AbstractView {
 
   getTemplate() {
     return createNoTasksTemplate;
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
