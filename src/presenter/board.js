fc6d612e7687497784cbe98ac6240c795aec5e1e
CptKokao <pro-markov@yandex.ru>
2020-09-18 20:46:09 +0300

    Добавляет Loading

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -2,6 +2,7 @@ import BoardView from '../view/board.js';
 import SortView from '../view/sort.js';
 import TaskListView from '../view/task-list.js';
 import NoTaskView from '../view/no-tasks.js';
+import LoadingView from "../view/loading.js";
 import LoadMoreButtonView from '../view/loader-more-button.js';
 import TaskPresenter from "./task.js";
 import TaskNewPresenter from "./task-new.js";
@@ -20,6 +21,7 @@ export default class Board {
     this._renderedTaskCount = TASK_COUNT_PER_STEP;
     this._currentSortType = SortType.DEFAULT;
     this._taskPresenter = {};
+    this._isLoading = true;
 
     this._sortComponent = null;
     this._loadMoreButtonComponent = null;
@@ -27,6 +29,7 @@ export default class Board {
     this._boardComponent = new BoardView();
     this._taskListComponent = new TaskListView();
     this._noTaskComponent = new NoTaskView();
+    this._loadingComponent = new LoadingView();
 
     this._handleViewAction = this._handleViewAction.bind(this);
     this._handleModelEvent = this._handleModelEvent.bind(this);
@@ -113,6 +116,11 @@ export default class Board {
         this._clearBoard({resetRenderedTaskCount: true, resetSortType: true});
         this._renderBoard();
         break;
+      case UpdateType.INIT:
+        this._isLoading = false;
+        remove(this._loadingComponent);
+        this._renderBoard();
+        break;
     }
   }
 
@@ -147,6 +155,10 @@ export default class Board {
   // Метод для рендеринга
   _renderTasks(tasks) {
     tasks.forEach((task) => this._renderTask(task));
+  }
+
+  _renderLoading() {
+    render(this._boardComponent, this._loadingComponent, RenderPosition.AFTERBEGIN);
   }
 
   // Метод для рендеринга заглушки
@@ -190,6 +202,7 @@ export default class Board {
 
     remove(this._sortComponent);
     remove(this._noTaskComponent);
+    remove(this._loadingComponent);
     remove(this._loadMoreButtonComponent);
 
     if (resetRenderedTaskCount) {
@@ -207,6 +220,11 @@ export default class Board {
   }
 
   _renderBoard() {
+    if (this._isLoading) {
+      this._renderLoading();
+      return;
+    }
+
     const tasks = this._getTasks();
     const taskCount = tasks.length;
 
